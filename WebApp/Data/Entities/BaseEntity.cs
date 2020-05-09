e81d844f7433f60c46b52972f31b362b0ad8fd43
Андрій Лесів <aner98lam@ukr.net>
2020-05-09 20:33:47 +0300

    AuthorId was added in base entity

--- WebApp/Data/Entities/BaseEntity.cs
+++ WebApp/Data/Entities/BaseEntity.cs
@@ -8,5 +8,6 @@ namespace WebApp.Data.Entities
 		public DateTime Created { get; set; }
 		public string Author { get; set; }
 		public string Content { get; set; }
+		public int AuthorId { get; set; }
 	}
 }
---
+++ WebApp/Data/Migrations/20200509173230_AddAuthorIdInBaseEntity.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApp.Data.Migrations
+{
+    public partial class AddAuthorIdInBaseEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId",
+                table: "Questions",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId",
+                table: "Comments",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId",
+                table: "Answers",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "Questions");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "Answers");
+        }
+    }
+}
