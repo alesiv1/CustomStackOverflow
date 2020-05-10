2909d150ac7c73053c701f8cad2d4befecbcb2c3
Андрій Лесів <aner98lam@ukr.net>
2020-05-10 22:28:10 +0300

    Question controller was updated

--- WebApp/Controllers/QuestionsController.cs
+++ WebApp/Controllers/QuestionsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApp.Data;
@@ -11,19 +12,19 @@ namespace WebApp.Controllers
     public class QuestionsController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public QuestionsController(ApplicationDbContext context)
+        public QuestionsController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
-        // GET: Question
         public async Task<IActionResult> Index()
         {
             return View(await _context.Questions.ToListAsync());
         }
 
-        // GET: Question/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -32,7 +33,11 @@ namespace WebApp.Controllers
             }
 
             var questionEntity = await _context.Questions
+                .Include(x => x.Answers)
+                .Include(x => x.Visitors)
+                .Include(x => x.Tags)
                 .FirstOrDefaultAsync(m => m.Id == id);
+
             if (questionEntity == null)
             {
                 return NotFound();
@@ -41,21 +46,18 @@ namespace WebApp.Controllers
             return View(questionEntity);
         }
 
-        // GET: Question/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Question/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Title,Views,Id,Created,Author,Content,AuthorId")] QuestionEntity questionEntity)
+        public async Task<IActionResult> Create([Bind("Title,Id,Content,Tags")] QuestionEntity questionEntity)
         {
             if (ModelState.IsValid)
             {
+                questionEntity = await CreateQuestion(questionEntity);
                 _context.Add(questionEntity);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
@@ -63,15 +65,18 @@ namespace WebApp.Controllers
             return View(questionEntity);
         }
 
-        // GET: Question/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
-
             var questionEntity = await _context.Questions.FindAsync(id);
+            var user = await GetCurentUser();
+            if (user.Id != questionEntity.AuthorId && User.IsInRole("Admin"))
+            {
+                return RedirectToAction(nameof(Index));
+            }
             if (questionEntity == null)
             {
                 return NotFound();
@@ -79,12 +84,9 @@ namespace WebApp.Controllers
             return View(questionEntity);
         }
 
-        // POST: Question/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Title,Views,Id,Created,Author,Content,AuthorId")] QuestionEntity questionEntity)
+        public async Task<IActionResult> Edit(int id, [Bind("Title,Id,Content,Tags")] QuestionEntity questionEntity)
         {
             if (id != questionEntity.Id)
             {
@@ -114,7 +116,6 @@ namespace WebApp.Controllers
             return View(questionEntity);
         }
 
-        // GET: Question/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -124,6 +125,11 @@ namespace WebApp.Controllers
 
             var questionEntity = await _context.Questions
                 .FirstOrDefaultAsync(m => m.Id == id);
+            var user = await GetCurentUser();
+            if (user.Id != questionEntity.AuthorId && User.IsInRole("Admin"))
+            {
+                return RedirectToAction(nameof(Index));
+            }
             if (questionEntity == null)
             {
                 return NotFound();
@@ -132,12 +138,25 @@ namespace WebApp.Controllers
             return View(questionEntity);
         }
 
-        // POST: Question/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var questionEntity = await _context.Questions.FindAsync(id);
+            var questionEntity = await _context.Questions
+                .Include(x => x.Answers)
+                .Include(x => x.Visitors)
+                .Include(x => x.Tags)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            foreach(var answer in questionEntity.Answers)
+            {
+                var answerEntity = await _context.Answers
+                    .Include(x => x.Comments)
+                    .FirstOrDefaultAsync(m => m.Id == id);
+                _context.Comments.RemoveRange(answerEntity.Comments);
+                _context.Answers.Remove(answerEntity);
+            }
+            _context.QuestionVisitors.RemoveRange(questionEntity.Visitors);
+            _context.QuestionTags.RemoveRange(questionEntity.Tags);
             _context.Questions.Remove(questionEntity);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
@@ -147,5 +166,20 @@ namespace WebApp.Controllers
         {
             return _context.Questions.Any(e => e.Id == id);
         }
+
+        private async Task<QuestionEntity> CreateQuestion(QuestionEntity questionEntity)
+        {
+            var user = await GetCurentUser();
+            questionEntity.Author = user.UserName;
+            questionEntity.AuthorId = user.Id;
+            questionEntity.Created = DateTime.Now;
+            return questionEntity;
+        }
+
+        private async Task<IdentityUser> GetCurentUser()
+        {
+            return await _userManager.GetUserAsync(HttpContext.User);
+        }
     }
 }
+ 
---
+++ WebApp/Data/Migrations/20200510123609_ChangeTypeForAuthorId.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApp.Data.Migrations
+{
+    public partial class ChangeTypeForAuthorId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorId",
+                table: "Questions",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorId",
+                table: "Comments",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorId",
+                table: "Answers",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "AuthorId",
+                table: "Questions",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AuthorId",
+                table: "Comments",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AuthorId",
+                table: "Answers",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
